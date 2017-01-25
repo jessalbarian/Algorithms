97d434bfe9936117277d0c5323e997ce22bb89c2
Jessie Albarian <albarian.jess@gmail.com>
2017-01-24 17:49:39 -0700

    added linear search

---
+++ linearsearch.py
@@ -0,0 +1,12 @@
+# Objective: Search through a list. Return the value if found, otherwise, return nil
+
+A = [1,2,3,4,5,6]
+v = 7
+
+def linearSearch(A, v):
+        position = 0
+        while position < len(A):
+                if A[position] == v:
+                        return v
+                position += 1
+        return
